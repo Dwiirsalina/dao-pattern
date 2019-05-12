139faa5d47d23ceb641c105721f5eea266790578
Dwi Irsalina <dwiirsalina25@gmail.com>
2019-05-12 12:24:30 +0700

    dao

---
+++ dao-pattern/src/test/java/com/coba/daopattern/DaoPatternApplicationTests.java
@@ -0,0 +1,16 @@
+package com.coba.daopattern;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DaoPatternApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
